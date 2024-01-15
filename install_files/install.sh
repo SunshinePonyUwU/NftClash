eb8cd1127897c3e3b5c52c89c0729070917a60f0
Sunshine <150380769+SunshinePonyUwU@users.noreply.github.com>
2024-01-15 08:13:19 +0800

    更新

--- install_files/install.sh
+++ install_files/install.sh
@@ -1,2 +1,3 @@
-cp /etc/nftclash/install/nftclashservice /etc/init.d/nftclash && chmod 755 /etc/init.d/nftclash && service nftclash enable
+
 cp /etc/nftclash/install/service.sh /etc/nftclash/service.sh && chmod 755 /etc/nftclash/service.sh
+cp /etc/nftclash/install/nftclashservice /etc/init.d/nftclash && chmod 755 /etc/init.d/nftclash && service nftclash enable
--- install_files/service.sh
+++ install_files/service.sh
@@ -5,23 +5,60 @@ CLASH_HOME_DIR=$DIR/clash
 CONFIG_PATH=$DIR/config.cfg
 
 init_config() {
-    source $CONFIG_PATH &> /dev/null
+  # source $CONFIG_PATH
+	tproxy_port=$(awk -F': ' '/tproxy-port/{print $2}' "$CLASH_HOME_DIR/config.yaml")
+	redir_port=$(awk -F': ' '/redir-port/{print $2}' "$CLASH_HOME_DIR/config.yaml")
+	fwmark=$redir_port
 }
 
-init_startup() {
+init_fw() {
+	nft add table inet nftclash
+	nft flush table inet nftclash
+	nft add chain inet nftclash prerouting { type filter hook prerouting priority 0 \; }
+	nft add rule inet nftclash prerouting meta l4proto { tcp, udp } mark set $fwmark tproxy to :$tproxy_port
 
+}
+
+init_startup() {\
+	! modprobe nft_tproxy && {
+		echo "missing nft_tproxy!!!"
+		exit 1
+	}
+	if [ -e "$CLASH_HOME_DIR/clash" ]; then
+		if [ -e "$CLASH_HOME_DIR/config.yaml" ]; then
+			chmod 777 -R "$DIR"
+			echo "INIT STARTUP DONE!"
+		else
+			echo "Please manually move the clash config file to $CLASH_HOME_DIR/config.yaml"
+		fi
+	else
+		echo "clash file does not exist!!!"
+		if [ ! -d "$CLASH_HOME_DIR" ]; then
+			echo "Creating directory"
+			mkdir -p "$CLASH_HOME_DIR"
+		fi
+		echo "Please manually move the clash executable file to $CLASH_HOME_DIR/clash"
+		exit 1
+	fi
 }
 
 init_started() {
-
+	init_config
+	init_fw
 }
 
 case "$1" in
-    init_startup)
+  init_startup)
 		init_startup
 	;;
-    init_started)
+  init_started)
 		init_started
+	;;
+	init_config)
+		init_config
+	;;
+	init_fw)
+		init_fw
 	;;
 esac
 exit 0
