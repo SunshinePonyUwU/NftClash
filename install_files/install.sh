1d05118d60331da885ceee335a0487c9bc3f571c
Sunshine <150380769+SunshinePonyUwU@users.noreply.github.com>
2024-01-16 04:46:49 +0800

    更新

--- install_files/install.sh
+++ install_files/install.sh
@@ -1,3 +1,4 @@
 cp /etc/nftclash/install/service.sh /etc/nftclash/service.sh && chmod 755 /etc/nftclash/service.sh
 cp /etc/nftclash/install/nftclashservice /etc/init.d/nftclash && chmod 755 /etc/init.d/nftclash && service nftclash enable
-cp /etc/nftclash/install/version /etc/nftclash/version
+[ ! -f /etc/nftclash/version ] && cp /etc/nftclash/install/version /etc/nftclash/version
+
