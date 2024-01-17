d1676cf14de740158e0ba42621e97e2ce7d37de6
Sunshine <150380769+SunshinePonyUwU@users.noreply.github.com>
2024-01-17 10:04:30 +0800

    Update install.sh

--- install_files/install.sh
+++ install_files/install.sh
@@ -1,3 +1,9 @@
+if ! command -v nft >/dev/null 2>&1; then
+	echo "Your system does not have nftables!!!"
+  echo "Cancel the installation."
+	exit 1
+fi
+
 cp /etc/nftclash/install/service.sh /etc/nftclash/service.sh && chmod 755 /etc/nftclash/service.sh
 cp /etc/nftclash/install/nftclashservice /etc/init.d/nftclash && chmod 755 /etc/init.d/nftclash && service nftclash enable
 [ ! -f /etc/nftclash/version ] && cp /etc/nftclash/install/version /etc/nftclash/version
