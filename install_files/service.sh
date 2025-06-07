80a10dbd2338e8b6e958496ea924d5c776b5e597
Sunshine <sunshine@20percent.cool>
2025-06-08 01:44:04 +0800

    fix sleep too long

--- install_files/service.sh
+++ install_files/service.sh
@@ -206,37 +206,34 @@ connection_check() {
       curl -x "socks5://127.0.0.1:$socks_port" -s "$CONN_CHECKS_URL"&> /dev/null
       if [ $? -eq 0 ]; then
         [ "$is_tproxy_chain_initialized" = "false" ] && {
-          CHECK_FAILURE=0
-          CHECK_FAILURE_COUNT=0
-          CHECK_SUCCESS=1
           CHECK_SUCCESS_COUNT=$(( CHECK_SUCCESS_COUNT + 1 ))
           if [ "$CHECK_SUCCESS_COUNT" -ge "$CONN_CHECKS_MIN_SUCCESSES" ]; then
             init_tproxy
             log_warn "socks5 test success, init tproxy. (x$CHECK_SUCCESS_COUNT)"
+            CHECK_FAILURE=0
+            CHECK_FAILURE_COUNT=0
+            CHECK_SUCCESS=1
           else
             log_warn "socks5 test success. (x$CHECK_SUCCESS_COUNT)"
           fi
         }
       else
         [ "$is_tproxy_chain_initialized" = "true" ] && {
-          CHECK_FAILURE=1
           CHECK_FAILURE_COUNT=$(( CHECK_FAILURE_COUNT + 1 ))
-          CHECK_SUCCESS=0
-          CHECK_SUCCESS_COUNT=0
           if [ "$CHECK_FAILURE_COUNT" -ge "$CONN_CHECKS_MAX_FAILURES" ]; then
             flush_tproxy
             log_warn "socks5 test failure, flush tproxy. (x$CHECK_FAILURE_COUNT)"
+            CHECK_FAILURE=1
+            CHECK_SUCCESS=0
+            CHECK_SUCCESS_COUNT=0
           else
             log_warn "socks5 test failure. (x$CHECK_FAILURE_COUNT)"
           fi
         }
       fi
 
-      if [ "$is_tproxy_chain_initialized" = "true" ]; then
-          sleep "$CONN_CHECKS_RETRY_INTERVAL"
-      else
-          sleep "$CONN_CHECKS_INTERVAL"
-      fi
+      sleep "$CONN_CHECKS_RETRY_INTERVAL"
+      sleep "$CONN_CHECKS_INTERVAL"
     done
   }
 }
